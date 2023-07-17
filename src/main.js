1004a807f5a721917edb5d6bb36d9bb822cd58e5
gabrielberarducci <gabrielberarducci@gmail.com>
2023-07-17 21:37:13 +0200

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
-
 import './assets/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from "bootstrap/dist/js/bootstrap.js";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.use(bootstrap).mount("#app");
